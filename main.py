80b4dfecdc8c061aef73fe41daeacc7f811755ce
nathakemi <91505104+nathakemi@users.noreply.github.com>
2021-09-29 20:33:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,58 @@
 entrada = str(input())
-while True:
-
-    if entrada == '--CONFIGURACAO':
+if entrada == '--CONFIGURACAO':
+    while entrada == '--CONFIGURACAO':
+        DADOS = []
         while True:
             AUX = input()
-            if AUX == '--ATENDIMENTO':
+            if AUX == "--ATENDIMENTO":
                 entrada = '--ATENDIMENTO'
                 break
             else:
                 a, m, c = AUX.split(' ')
 
-    if entrada == '--ATENDIMENTO':
-        while True:
-            AUX2 = input()
+            JA_ADICIONOU = False
+            i = 0
+            while i < len(DADOS):
+                if a == DADOS[i][0]:
+                    JA_ADICIONOU = True
+                    break
+                else:
+                    JA_ADICIONOU = False
+                i = i + 1
 
-            if AUX2 == -1:
-                entrada = -1
-                break
+            if JA_ADICIONOU:
+                DADOS[i] = DADOS[i] + [[m, c]]
+            else:
+                DADOS = DADOS + [[a, [m, c]]]
+        print('essse é o dado:', DADOS)
+        print('essa é a entrada:', entrada)
 
-            elif AUX2 == 1:
-                reserva = str(input())
-                pessoas = reserva[12]
-                area = reserva[-2]
-                cadeiras = reserva[-20]
+if entrada == '--ATENDIMENTO':
+    while True:
+        AUX2 = int(input())
 
-    if entrada == -1:
-        print('Bom descanso!')
-        break
+        if AUX2 == -1:
+            entrada = -1
+            print('Restaurante fechado.')
+            print('Balanco final de mesas:')
+            for area in DADOS:
+                print('essas são as mesas:', area)
+            print('Bom descanso!')
+            break
 
+        elif AUX2 == 1:
+            reserva = str(input())
+            pessoas = reserva[20]
+            area = reserva[38:]
+            print('essas são as pessoas:', pessoas)
+            print('essa é a área:', area)
+            if pessoas == c: #TA ERRADO PQ PEGA O ULTIMO C BURRA
+                cadeiras = pessoas
+                print('Um grupo de ', pessoas, ' pessoas ocupou uma mesa de', cadeiras, 'lugares na area', area,'.')
+                #cadeiras é maior ou igual a pessoas
+                # um if aqui
+            elif c < pessoas: #TA ERRADO PQ PEGA O ULTIMO C BURRA
+                print('não tem lugar')
+
+            else:
+                print('Um grupo de ', pessoas, ' pessoas ocupou uma mesa de', c, 'lugares na area', area, '.')
